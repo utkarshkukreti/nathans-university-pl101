2256eda50ef239828f71ce6d823c7b1d70fcce16
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-04-16 20:30:24 +0530

    Add music3.html : "Compiling Music for reals" in Haskell.

---
+++ music-haskell/compiler.hs
@@ -0,0 +1,24 @@
+data Expression =
+    Seq { left :: Expression, right :: Expression }
+  | Note { pitch :: String, duration :: Integer }
+  deriving (Show)
+
+compile :: Expression -> [Expression]
+compile (Seq { left = left, right = right }) = compile left ++ compile right
+compile (Note { pitch = pitch, duration = duration }) = [Note { pitch = pitch, duration = duration }]
+
+toCompile = Seq {
+    left = Seq {
+        left = Note { pitch = "a4", duration = 250 },
+        right = Note { pitch = "b4", duration = 250 }
+      },
+    right = Seq {
+        left = Note { pitch = "c4", duration = 500 },
+        right = Note { pitch = "d4", duration = 500 }
+      }
+  }
+
+
+main = do
+  putStrLn $ show toCompile
+  putStrLn $ show $ compile toCompile
