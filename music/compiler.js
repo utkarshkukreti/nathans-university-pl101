b0c45c17edfa1d8b480816bb7a3a3630b5b8c6ee
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-04-16 20:29:04 +0530

    Add music3.html: "Compiling Music for reals" in JS

---
+++ music/compiler.js
@@ -0,0 +1,24 @@
+var compile = function(expr) {
+  if(expr.tag === 'seq') {
+    var compiled = [];
+    compiled = compiled.concat(compile(expr.left));
+    compiled = compiled.concat(compile(expr.right));
+    return compiled;
+  } else if(expr.tag === 'note') {
+    return [expr];
+  }
+};
+
+var melody_music =
+    { tag: 'seq',
+      left:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', duration: 250 },
+         right: { tag: 'note', pitch: 'b4', duration: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', duration: 500 },
+         right: { tag: 'note', pitch: 'd4', duration: 500 } } };
+
+console.log(melody_music);
+console.log(compile(melody_music));
