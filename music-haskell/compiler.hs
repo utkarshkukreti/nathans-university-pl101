92ab2da3ca3f78389a0d9a739f88b6a5880db118
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-04-16 21:25:22 +0530

    Add music6.html : "Parallel Composition" in Haskell.

--- music-haskell/compiler.hs
+++ music-haskell/compiler.hs
@@ -1,22 +1,21 @@
 data Expression =
     Seq { left :: Expression, right :: Expression }
-  | Note { pitch :: String, duration :: Integer }
+  | Note { pitch :: String, duration :: Integer, start :: Integer }
+  | Par { left :: Expression, right :: Expression }
   deriving (Show)
 
-compile :: Expression -> [Expression]
-compile (Seq { left = left, right = right }) = compile left ++ compile right
-compile (Note { pitch = pitch, duration = duration }) = [Note { pitch = pitch, duration = duration }]
+endTime :: Expression -> Integer -> Integer
+endTime (Note { duration = duration }) start = duration + start
+endTime (Seq { left = left, right = right }) start = endTime right (endTime left start)
+endTime (Par { left = left, right = right }) start = max (endTime left start) (endTime right start)
 
-toCompile = Seq {
-    left = Seq {
-        left = Note { pitch = "a4", duration = 250 },
-        right = Note { pitch = "b4", duration = 250 }
-      },
-    right = Seq {
-        left = Note { pitch = "c4", duration = 500 },
-        right = Note { pitch = "d4", duration = 500 }
-      }
-  }
+compile :: Expression -> [Expression]
+compile expression = compile' expression 0
+  where
+    compile' :: Expression -> Integer -> [Expression]
+    compile' (Seq { left = left, right = right }) start = compile' left start ++ compile' right (endTime left start)
+    compile' (Par { left = left, right = right }) start = compile' left start ++ compile' right start
+    compile' (Note { pitch = pitch, duration = duration }) start = [Note { pitch = pitch, duration = duration, start = start }]
 
 playNote :: [Expression] -> [Expression]
 playNote expression = expression
@@ -24,6 +23,30 @@ playNote expression = expression
 playMusic :: Expression -> [Expression]
 playMusic = playNote . compile
 
+toCompileSeq = Seq {
+    left = Seq {
+        left = Note { pitch = "a4", duration = 250, start = 0 },
+        right = Note { pitch = "b4", duration = 250, start = 0 }
+      },
+    right = Seq {
+        left = Note { pitch = "c4", duration = 500, start = 0 },
+        right = Note { pitch = "d4", duration = 500, start = 0 }
+      }
+  }
+
+toCompilePar = Seq {
+    left = Par {
+        left = Note { pitch = "c3", duration = 250, start = 0 },
+        right = Note { pitch = "g4", duration = 500, start = 0 }
+      },
+    right = Par {
+        left = Note { pitch = "d3", duration = 500, start = 0 },
+        right = Note { pitch = "f4", duration = 250, start = 0}
+      }
+  }
+
 main = do
-  putStrLn $ show toCompile
-  putStrLn $ show $ compile toCompile
+  putStrLn $ show toCompileSeq
+  putStrLn $ show $ compile toCompileSeq
+  putStrLn $ show toCompilePar
+  putStrLn $ show $ compile toCompilePar
